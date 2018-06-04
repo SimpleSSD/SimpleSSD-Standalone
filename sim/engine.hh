621e664e7d4e3656959978c5635db17528226cc1
kukdh1 <kukdh1@gmail.com>
2018-06-04 13:43:35 +0900

    add simulator engine

---
+++ sim/engine.hh
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 CAMELab
+ *
+ * This file is part of SimpleSSD.
+ *
+ * SimpleSSD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleSSD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimpleSSD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifndef __SIM_ENGINE__
+#define __SIM_ENGINE__
+
+#include <list>
+#include <unordered_map>
+
+#include "simplessd/sim/simulator.hh"
+
+class Engine : SimpleSSD::Simulator {
+ private:
+  uint64_t simTick;
+  SimpleSSD::Event counter;
+  std::unordered_map<SimpleSSD::Event, SimpleSSD::EventFunction> eventList;
+  std::list<std::pair<SimpleSSD::Event, uint64_t>> eventQueue;
+
+  bool insertEvent(SimpleSSD::Event, uint64_t, uint64_t * = nullptr);
+  bool removeEvent(SimpleSSD::Event);
+  bool isEventExist(SimpleSSD::Event);
+
+ public:
+  Engine();
+  ~Engine();
+
+  uint64_t getCurrentTick() override;
+
+  SimpleSSD::Event allocateEvent(SimpleSSD::EventFunction) override;
+  void scheduleEvent(SimpleSSD::Event, uint64_t) override;
+  void descheduleEvent(SimpleSSD::Event) override;
+  bool isScheduled(SimpleSSD::Event) override;
+  void deallocateEvent(SimpleSSD::Event) override;
+
+  bool doNextEvent();
+};
+
+#endif
