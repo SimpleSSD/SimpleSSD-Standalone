15678c9b68e6f704fab1a949bfd0a65e4113227e
kukdh1 <kukdh1@gmail.com>
2018-06-04 14:12:26 +0900

    add main

--- sim/main.cc
+++ sim/main.cc
@@ -17,8 +17,47 @@
  * along with SimpleSSD.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "sim/main.hh"
+#include <iostream>
+
+#include "simplessd/util/simplessd.hh"
+#include "sim/engine.hh"
 
 int main(int argc, char *argv[]) {
+  Engine engine;
+  ConfigReader simConfig;
+
+  std::cout << "SimpleSSD Standalone v2.1" << std::endl;
+
+  // Check argument
+  if (argc != 3) {
+    std::cerr << " Invalid number of argument!" << std::endl;
+    std::cerr << "  Usage: simplessd-standalone <Simulation configuration "
+                 "file> <SimpleSSD configuration file>"
+              << std::endl;
+
+    return 1;
+  }
+
+  // Initialize SimpleSSD
+  auto ssdConfig = initSimpleSSDEngine(&engine, std::cout, std::cerr, argv[2]);
+
+  // Read simulation config file
+  if (!simConfig.init(argv[1])) {
+    std::cerr << " Failed to open simulation configuration file!" << std::endl;
+
+    return 2;
+  }
+
+  // Create interface
+  // TODO: fill here
+
+  // Do Simulation
+  while (engine.doNextEvent()) {
+    // Do nothing
+  }
+
+  std::cout << "End of simulation @ tick " << engine.getCurrentTick()
+            << std::endl;
+
   return 0;
 }
