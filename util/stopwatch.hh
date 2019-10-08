f59f45ac3718e03136a5e74c893371041b7539fa
Donghyun Gouk <kukdh1@gmail.com>
2019-10-09 00:46:27 +0900

    all: Update license

--- util/stopwatch.hh
+++ util/stopwatch.hh
@@ -1,21 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 /*
- * Copyright (C) 2017 CAMELab
+ * Copyright (C) 2019 CAMELab
  *
- * This file is part of SimpleSSD.
- *
- * SimpleSSD is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SimpleSSD is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SimpleSSD.  If not, see <http://www.gnu.org/licenses/>.
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
  */
+
+#pragma once
 
 #ifndef __UTIL_STOPWATCH__
 #define __UTIL_STOPWATCH__
