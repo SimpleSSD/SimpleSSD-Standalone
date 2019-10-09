7b05130a6e75c3fa4f77e28e07709789927e3b74
Donghyun Gouk <kukdh1@gmail.com>
2019-10-09 09:30:55 +0900

    sim: Add sim config reader

---
+++ sim/config.cc
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#include "sim/config.hh"
+
+#include <cstring>
+
+const char NAME_MODE[] = "Mode";
+const char NAME_LOG_PERIOD[] = "LogPeriod";
+const char NAME_OUTPUT_DIRECTORY[] = "OutputDirectory";
+const char NAME_LOG_FILE[] = "StatLogFile";
+const char NAME_DEBUG_LOG_FILE[] = "DebugLogFile";
+const char NAME_LATENCY_LOG_FILE[] = "LatencyLogFile";
+const char NAME_PROGRESS_PERIOD[] = "ProgressPeriod";
+const char NAME_INTERFACE[] = "Interface";
+const char NAME_SCHEDULER[] = "Scheduler";
+const char NAME_SUBMISSION_LATENCY[] = "SubmissionLatency";
+const char NAME_COMPLETION_LATENCY[] = "CompletionLatency";
+
+//! A constructor
+Config::Config() {
+  mode = ModeType::RequestGenerator;
+  statPeriod = 0;
+  outputDirectory = ".";
+  statFile = "stats.txt";
+  debugFile = "debug.txt";
+  latencyFile = "latency.txt";
+  progressPeriod = 0;
+  interface = InterfaceType::None;
+  scheduler = SchedulerType::Noop;
+  submissionLatency = 0;
+  completionLatency = 0;
+}
+
+//! A destructor
+Config::~Config() {}
+
+void Config::loadFrom(pugi::xml_node &section) {
+  for (auto node = section.first_child(); node; node = node.next_sibling()) {
+    LOAD_NAME_UINT_TYPE(node, NAME_MODE, ModeType, mode);
+    LOAD_NAME_TIME(node, NAME_LOG_PERIOD, statPeriod);
+    LOAD_NAME_STRING(node, NAME_OUTPUT_DIRECTORY, outputDirectory);
+    LOAD_NAME_STRING(node, NAME_LOG_FILE, statFile);
+    LOAD_NAME_STRING(node, NAME_DEBUG_LOG_FILE, debugFile);
+    LOAD_NAME_STRING(node, NAME_LATENCY_LOG_FILE, latencyFile);
+    LOAD_NAME_UINT(node, NAME_PROGRESS_PERIOD, progressPeriod);
+    LOAD_NAME_UINT_TYPE(node, NAME_INTERFACE, InterfaceType, interface);
+    LOAD_NAME_UINT_TYPE(node, NAME_SCHEDULER, SchedulerType, scheduler);
+    LOAD_NAME_TIME(node, NAME_SUBMISSION_LATENCY, submissionLatency);
+    LOAD_NAME_TIME(node, NAME_COMPLETION_LATENCY, completionLatency);
+  }
+}
+
+void Config::storeTo(pugi::xml_node &section) {
+  // Assume section node is empty
+  STORE_NAME_UINT(section, NAME_MODE, mode);
+  STORE_NAME_TIME(section, NAME_LOG_PERIOD, statPeriod);
+  STORE_NAME_STRING(section, NAME_OUTPUT_DIRECTORY, outputDirectory);
+  STORE_NAME_STRING(section, NAME_LOG_FILE, statFile);
+  STORE_NAME_STRING(section, NAME_DEBUG_LOG_FILE, debugFile);
+  STORE_NAME_STRING(section, NAME_LATENCY_LOG_FILE, latencyFile);
+  STORE_NAME_UINT(section, NAME_PROGRESS_PERIOD, progressPeriod);
+  STORE_NAME_UINT(section, NAME_INTERFACE, interface);
+  STORE_NAME_UINT(section, NAME_SCHEDULER, scheduler);
+  STORE_NAME_TIME(section, NAME_SUBMISSION_LATENCY, submissionLatency);
+  STORE_NAME_TIME(section, NAME_COMPLETION_LATENCY, completionLatency);
+}
+
+uint64_t Config::readUint(uint32_t idx) {
+  switch (idx) {
+    case Key::Mode:
+      return (uint64_t)mode;
+    case Key::StatPeriod:
+      return statPeriod;
+    case Key::ProgressPeriod:
+      return progressPeriod;
+    case Key::Interface:
+      return (uint64_t)interface;
+    case Key::Scheduler:
+      return (uint64_t)scheduler;
+    case Key::SubmissionLatency:
+      return submissionLatency;
+    case Key::CompletionLatency:
+      return completionLatency;
+  }
+
+  return 0;
+}
+
+std::string Config::readString(uint32_t idx) {
+  switch (idx) {
+    case Key::OutputDirectory:
+      return outputDirectory;
+    case Key::StatFile:
+      return statFile;
+    case Key::DebugFile:
+      return debugFile;
+    case Key::Latencyfile:
+      return latencyFile;
+  }
+
+  return "";
+}
+
+bool Config::writeUint(uint32_t idx, uint64_t value) {
+  bool ret = true;
+
+  switch (idx) {
+    case Key::Mode:
+      mode = (ModeType)value;
+    case Key::StatPeriod:
+      statPeriod = value;
+    case Key::ProgressPeriod:
+      progressPeriod = value;
+    case Key::Interface:
+      interface = (InterfaceType)value;
+    case Key::Scheduler:
+      scheduler = (SchedulerType)value;
+    case Key::SubmissionLatency:
+      submissionLatency = value;
+    case Key::CompletionLatency:
+      completionLatency = value;
+    default:
+      ret = false;
+      break;
+  }
+
+  return ret;
+}
+
+bool Config::writeString(uint32_t idx, std::string &value) {
+  bool ret = true;
+
+  switch (idx) {
+    case Key::OutputDirectory:
+      outputDirectory = value;
+      break;
+    case Key::StatFile:
+      statFile = value;
+    case Key::DebugFile:
+      debugFile = value;
+    case Key::Latencyfile:
+      latencyFile = value;
+    default:
+      ret = false;
+      break;
+  }
+
+  return ret;
+}
---
+++ sim/config.hh
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __SIM_CONFIG_HH__
+#define __SIM_CONFIG_HH__
+
+#include "simplessd/sim/base_config.hh"
+
+/**
+ * \brief SimConfig object declaration
+ *
+ * Stores simulation configurations such as simulation output directory.
+ */
+class Config : public SimpleSSD::BaseConfig {
+ public:
+  enum Key : uint32_t {
+    Mode,
+    StatPeriod,
+    OutputDirectory,
+    StatFile,
+    DebugFile,
+    Latencyfile,
+    ProgressPeriod,
+    Interface,
+    Scheduler,
+    SubmissionLatency,
+    CompletionLatency,
+  };
+
+  enum class ModeType : uint8_t {
+    RequestGenerator,
+    TraceReplayer,
+  };
+
+  enum class InterfaceType : uint8_t {
+    None,
+    NVMe,
+  };
+
+  enum class SchedulerType : uint8_t {
+    Noop,
+  };
+
+ private:
+  ModeType mode;
+  uint64_t statPeriod;
+  std::string outputDirectory;
+  std::string statFile;
+  std::string debugFile;
+  std::string latencyFile;
+  uint64_t progressPeriod;
+  InterfaceType interface;
+  SchedulerType scheduler;
+  uint64_t submissionLatency;
+  uint64_t completionLatency;
+
+ public:
+  Config();
+  ~Config();
+
+  const char *getSectionName() override { return "sim"; }
+
+  void loadFrom(pugi::xml_node &) override;
+  void storeTo(pugi::xml_node &) override;
+
+  uint64_t readUint(uint32_t) override;
+  std::string readString(uint32_t) override;
+  bool writeUint(uint32_t, uint64_t) override;
+  bool writeString(uint32_t, std::string &) override;
+};
+
+#endif
