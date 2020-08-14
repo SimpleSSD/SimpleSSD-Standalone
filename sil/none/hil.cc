2344e9590441abb288faabe4bb71543c9c2bd3d1
Donghyun Gouk <kukdh1@gmail.com>
2020-08-14 10:58:25 +0900

    sil/none: Add None interface

---
+++ sil/none/hil.cc
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#include "sil/none/hil.hh"
+
+#include "simplessd/hil/none/controller.hh"
+
+namespace Standalone::SIL::None {
+
+Driver::Driver(ObjectData &o, SimpleSSD::SimpleSSD &s)
+    : BIL::DriverInterface(o, s), pHIL(nullptr) {
+  auto id = simplessd.createController(this);
+  auto controller = dynamic_cast<SimpleSSD::HIL::None::Controller *>(
+      simplessd.getController(id));
+
+  panic_if(controller == nullptr, "No controller?");
+
+  pHIL = controller->getHIL();
+
+  completionEvent = simplessd.getObject().cpu->createEvent(
+      [this](uint64_t t, uint64_t d) { complete(t, d); },
+      "Standalone::SIL::None::completionEvent");
+}
+
+Driver::~Driver() {}
+
+void Driver::init(Event eid) {
+  info("Initialization finished");
+
+  scheduleNow(eid);
+}
+
+void Driver::getInfo(uint64_t &bytesize, uint32_t &minbs) {
+  bytesize = pHIL->getTotalPages();
+  minbs = pHIL->getLPNSize();
+}
+
+void Driver::submitIO(BIL::BIO &bio) {
+  auto ret = requestQueue.emplace(
+      bio.id, SimpleSSD::HIL::Request(completionEvent, bio.id));
+
+  panic_if(!ret.second, "BIO ID conflict!");
+
+  auto &request = ret.first->second;
+
+  request.setAddress(bio.offset >> 9, bio.length >> 9, 512);
+  request.setHostTag(bio.id);
+
+  switch (bio.type) {
+    case BIL::BIOType::Read:
+      pHIL->read(&request);
+
+      break;
+    case BIL::BIOType::Write:
+      pHIL->write(&request);
+
+      break;
+    case BIL::BIOType::Flush:
+      pHIL->flush(&request);
+
+      break;
+    case BIL::BIOType::Trim:
+      pHIL->format(&request, SimpleSSD::HIL::FormatOption::None);
+
+      break;
+    default:
+      panic("Unexpected I/O type.");
+
+      break;
+  }
+}
+
+void Driver::complete(uint64_t, uint64_t) {}
+
+void Driver::read(uint64_t, uint32_t, uint8_t *, SimpleSSD::Event, uint64_t) {
+  panic("Calling not implemented function.");
+}
+
+void Driver::write(uint64_t, uint32_t, uint8_t *, SimpleSSD::Event, uint64_t) {
+  panic("Calling not implemented function.");
+}
+
+void Driver::postInterrupt(uint16_t, bool) {
+  panic("Calling not implemented function.");
+}
+
+void Driver::getPCIID(uint16_t &, uint16_t &) {
+  panic("Calling not implemented function.");
+}
+
+void Driver::initStats(std::vector<SimpleSSD::Stat> &list) {
+  simplessd.getStatList(list, "");
+}
+
+void Driver::getStats(std::vector<double> &values) {
+  simplessd.getStatValues(values);
+}
+
+}  // namespace Standalone::SIL::None
---
+++ sil/none/hil.hh
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __DRIVERS_HIL__
+#define __DRIVERS_HIL__
+
+#include <vector>
+#include <unordered_map>
+
+#include "bil/interface.hh"
+#include "simplessd/hil/hil.hh"
+
+namespace Standalone::SIL::None {
+
+class Driver : public BIL::DriverInterface, SimpleSSD::Interface {
+ private:
+  SimpleSSD::HIL::HIL *pHIL;
+
+  std::unordered_map<uint64_t, SimpleSSD::HIL::Request> requestQueue;
+
+  SimpleSSD::Event completionEvent;
+
+  void complete(uint64_t, uint64_t);
+
+ public:
+  Driver(ObjectData &, SimpleSSD::SimpleSSD &);
+  ~Driver();
+
+  // BIL::DriverInterface
+  void init(Event) override;
+  void getInfo(uint64_t &, uint32_t &) override;
+  void submitIO(BIL::BIO &) override;
+
+  // SimpleSSD::Interface
+  void read(uint64_t, uint32_t, uint8_t *, SimpleSSD::Event,
+            uint64_t = 0) override;
+  void write(uint64_t, uint32_t, uint8_t *, SimpleSSD::Event,
+             uint64_t = 0) override;
+
+  void postInterrupt(uint16_t, bool) override;
+  void getPCIID(uint16_t &, uint16_t &) override;
+
+  void initStats(std::vector<SimpleSSD::Stat> &) override;
+  void getStats(std::vector<double> &) override;
+};
+
+}  // namespace Standalone::SIL::None
+
+#endif
