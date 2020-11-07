4d87d3cd5538cb87e314e5452299bfe9f201701e
Donghyun Gouk <kukdh1@gmail.com>
2020-11-07 14:46:42 +0900

    util: Update argparser

--- util/argparse.cc
+++ util/argparse.cc
@@ -12,8 +12,20 @@
 namespace Standalone {
 
 static const std::regex regex_arg("(-\\w|--[\\w-]+)(?:=(.*))?");
+static const char str_true[] = "true";
 
-ArgumentParser::ArgumentParser(int argc, char *argv[]) : valid(true) {
+ArgumentParser::ArgumentParser(int argc, char *argv[]) : valid(true), pos(0) {
+  parse(argc, argv);
+}
+
+ArgumentParser::ArgumentParser(int argc, char *argv[], int p)
+    : valid(true), pos(p) {
+  parse(argc, argv);
+}
+
+ArgumentParser::~ArgumentParser() {}
+
+void ArgumentParser::parse(int argc, char *argv[]) {
   bool value = false;
   std::cmatch match;
   std::string key;
@@ -21,36 +33,78 @@ ArgumentParser::ArgumentParser(int argc, char *argv[]) : valid(true) {
   for (int i = 1; i < argc; i++) {
     char *param = argv[i];
 
-    if (value == false && std::regex_match(param, match, regex_arg)) {
-      key = match[1].str();
+    if (param[0] != '-') {
+      if (value) {
+        if (key.length() == 0) {
+          goto out;
+        }
 
-      if (match.length() > 2 && match[2].matched) {
-        args.emplace(key, match[2].first);
+        value = false;
+
+        args.emplace(key, param);
+      }
+      else if (pos > 0) {
+        pos--;
+        posargs.emplace_back(param);
       }
       else {
-        value = true;
+        goto out;
       }
     }
-    else if (value && key.length() > 0) {
-      value = false;
-
-      args.emplace(key, param);
-    }
     else {
-      valid = false;
+      if (strcmp(param, "--") == 0) {
+        // Assume positional arguments after this
+        for (int j = i + 1; j < argc; j++) {
+          if (pos > 0) {
+            pos--;
+            posargs.emplace_back(argv[j]);
+          }
+          else {
+            goto out;
+          }
+        }
 
-      break;
+        break;
+      }
+      else if (std::regex_match(param, match, regex_arg)) {
+        if (value) {
+          // We got two consecutive options
+          args.emplace(key, str_true);
+        }
+
+        key = match[1].str();
+
+        if (match.length() > 2 && match[2].matched) {
+          args.emplace(key, match[2].first);
+        }
+        else {
+          value = true;
+        }
+      }
+      else {
+        goto out;
+      }
     }
   }
-}
 
-ArgumentParser::~ArgumentParser() {}
+  return;
+out:
+  valid = false;
+}
 
 const char *ArgumentParser::getArgument(std::string key) noexcept {
   auto iter = args.find(key);
 
   if (iter != args.end()) {
     return iter->second;
+  }
+
+  return nullptr;
+}
+
+const char *ArgumentParser::getPositionalArgument(int pos) noexcept {
+  if (pos < posargs.size()) {
+    return posargs.at(pos);
   }
 
   return nullptr;
--- util/argparse.hh
+++ util/argparse.hh
@@ -13,20 +13,27 @@
 #include <cinttypes>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace Standalone {
 
 class ArgumentParser {
  private:
   bool valid;
+  int pos;
   std::unordered_map<std::string, const char *> args;
+  std::vector<const char *> posargs;
+
+  void parse(int, char *[]);
 
  public:
   ArgumentParser(int, char *[]);
+  ArgumentParser(int, char *[], int);
   ~ArgumentParser();
 
   bool isValid() noexcept { return valid; }
   const char *getArgument(std::string) noexcept;
+  const char *getPositionalArgument(int) noexcept;
 };
 
 }  // namespace Standalone
